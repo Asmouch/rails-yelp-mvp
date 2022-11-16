5dbd8764f6a2929322713e52caaf2ccd4e399f01
asma chouar <asmachouar@hotmail.fr>
2022-11-16 17:11:17 +0100

    exo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+otacos = {name: "O'tacos", address: "10 rue toto", category: "chinese"}
+pitaya = {name: "Pitaya", address: "10 rue titi", category: "italian"}
+fresh_burger = {name: "fresh burger", address: "10 rue tutu", category: "french"}
+mcdo = {name: "Mcdo", address: "10 rue tata", category: "japanese"}
+olafritte = {name: "O'lafritte", address: "10 rue tete", category: "belgian"}
+
+[otacos, pitaya, fresh_burger, mcdo, olafritte].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
